a857cc5f0721d563caac585c1a593f7475d0c2cd
JPGAGHAN <jpgaghan@gmail.com>
2018-09-01 09:24:18 -0400

    mostapisworking

--- liri.js
+++ liri.js
@@ -1,18 +1,53 @@
-var env = require("dotenv").config();
 
-var keys = import("keys.js");
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
 var request = require('request')
 var command = process.argv[2];
+var argument= process.argv[3];
+var bandsintown = require('bandsintown')("codingbootcamp");
 
-// if (command === "concert-this") {
-    request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+for (i = 4; i<process.argv.length; i++) {
+    argument += `+ ${process.argv[i]}`
+}
+
+if (command === "movie-this") {
+    request(`http://www.omdbapi.com/?t=${argument}&y=&plot=short&apikey=trilogy`, function(error, response, body) {
 
   if (!error && response.statusCode === 200) {
 
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+    console.log(JSON.parse(body).Title);
+    console.log(JSON.parse(body).Year);
+    console.log(JSON.parse(body).imdbRating);
+    console.log(JSON.parse(body).Ratings[1].Value);
+    console.log(JSON.parse(body).Country);
+    console.log(JSON.parse(body).Language);
+    console.log(JSON.parse(body).Plot);
+    console.log(JSON.parse(body).Actors);
   }
 });
-// }
+} else if (command === "concert-this") {
+    bandsintown.getArtistEventList(argument)
+  .then(function(events) {
+    console.log(events)
+  });
+} else if (command === "spotify-this-song") {
+    spotify.search({type: 'track', query: argument, limit: 1}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    trackdata = data.tracks.items[0]
+  console.log(trackdata.album.name); 
+  console.log(trackdata.album.artists[0].name); 
+  console.log(trackdata.preview_url)
+  console.log(trackdata.name); 
+
+
+
+  });
+}
